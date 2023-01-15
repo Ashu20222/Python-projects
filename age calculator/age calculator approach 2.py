7301af560862fcb8d7a65fd55b106df8f3dde96e
ashutosh gole <95989385+Ashu20222@users.noreply.github.com>
2023-01-15 22:10:39 +0530

    Create age calculator approach 2.py

---
+++ age calculator/age calculator approach 2.py
@@ -0,0 +1,18 @@
+from datetime import date
+
+def age_calculator():
+    # Get birthdate from user
+    birth_year = int(input("Enter your birth year: "))
+    birth_month = int(input("Enter your birth month: "))
+    birth_day = int(input("Enter your birth day: "))
+
+    # Get current date
+    today = date.today()
+
+    # Calculate age
+    age = today.year - birth_year
+    if today.month < birth_month or (today.month == birth_month and today.day < birth_day):
+        age = age
+
+    # Print age
+    print("Your age is: ", age)
